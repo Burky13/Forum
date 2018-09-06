1f19297b0292b719a7e400628e6b004c9cc8669a
dondo24 <o.daniel1995@gmail.com>
2018-09-06 09:56:01 +0200

    Entity User User service and UserServiceJpa

--- forum/src/main/java/forum/SpringMain.java
+++ forum/src/main/java/forum/SpringMain.java
@@ -1,13 +1,24 @@
 package forum;
 
+import forum.services.UserService;
+import forum.services.UserServiceJpa;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
+@Configuration
 public class SpringMain {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
-        System.out.println("hello");
     }
+        @Bean
+        public UserService userService() {
+            return new UserServiceJpa();
+        }
+
 }
 
 
