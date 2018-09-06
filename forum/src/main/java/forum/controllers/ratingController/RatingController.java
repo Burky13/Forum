3ce5e218ac1531bfc1b39c7ee8cf4d40fe371892
PStofa <stofa.patrik@gmail.com>
2018-09-06 14:33:43 +0200

    rating control

---
+++ forum/src/main/java/forum/controllers/ratingController/RatingController.java
@@ -0,0 +1,29 @@
+package forum.controllers.ratingController;
+
+import forum.Entity.Rating;
+import forum.services.ratingService.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+    @Autowired
+    private UserController userController;
+
+    public static void main(String[] args){
+
+    }
+
+    @RequestMapping("/rate")
+    public String changeRating(Rating rating){
+        if(rating.getValue() != 0 && userController.isLogged()){
+            rating.setUserName(userController.getLoggedUserName());
+            ratingService.changeRating(rating);
+        }
+        return rating.getCommentId();
+    }
+}
