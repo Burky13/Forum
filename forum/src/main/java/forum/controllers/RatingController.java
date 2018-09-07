86dc2c0aa7995f334bd6eb2d3ec04e41182a178b
PStofa <stofa.patrik@gmail.com>
2018-09-07 14:52:01 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	forum/src/main/java/forum/controllers/RatingController.java

--- forum/src/main/java/forum/controllers/RatingController.java
+++ forum/src/main/java/forum/controllers/RatingController.java
@@ -3,10 +3,13 @@ package forum.controllers;
 import forum.entity.Rating;
 import forum.services.rating.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 
 @Controller
+@Scope(WebApplicationContext.SCOPE_SESSION)
 public class RatingController {
 
     @Autowired
