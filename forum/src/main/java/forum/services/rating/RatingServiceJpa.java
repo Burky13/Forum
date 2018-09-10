b84e1417e53fb9bfc6361e6c23d9533a6ef5d557
PStofa <37082213+PStofa@users.noreply.github.com>
2018-09-10 13:04:12 +0200

    Update RatingServiceJpa.java

--- forum/src/main/java/forum/services/rating/RatingServiceJpa.java
+++ forum/src/main/java/forum/services/rating/RatingServiceJpa.java
@@ -1,11 +1,13 @@
 package forum.services.rating;
 
+import forum.entity.Comment;
 import forum.entity.Rating;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public class RatingServiceJpa implements RatingService{
@@ -14,18 +16,26 @@ public class RatingServiceJpa implements RatingService{
     private EntityManager entityManager;
 
     @Override
-    public void changeRating(Rating rating){
+    public void changeRating(Rating rating) {
         Rating r = null;
+        Comment c = null;
         try {
             r = entityManager.createQuery("Select r from Rating r where r.userName = :username and r.commentId = :commentid", Rating.class)
-            .setParameter("username", rating.getUserName()).setParameter("commentid", rating.getCommentId())
-            .getSingleResult();
-        } catch (NoResultException e){ }
-        if (r != null){
+                    .setParameter("username", rating.getUserName()).setParameter("commentid", rating.getCommentId())
+                    .getSingleResult();
+        } catch (NoResultException e) {
+        }
+        if (r != null) {
             r.setValue(rating.getValue());
-        }else{
+        } else {
             entityManager.persist(rating);
         }
+        try {
+            c = entityManager.createQuery("Select c from Comment c where r.id = :commentid and r.userName = :username", Comment.class)
+                    .setParameter("commentid", rating.getCommentId()).setParameter("username", rating.getPoster()).getSingleResult();
+        } catch (NoResultException e) { }
+        c.setRating(getAvgRating(c.getTopicId(),"comment"));
+        entityManager.merge(c);
     }
 
     @Override
@@ -43,9 +53,19 @@ public class RatingServiceJpa implements RatingService{
         }catch (NoResultException e){}
 
         if(likes == 0 && dislikes == 0){
-            return 50;
+            return 0.5;
         }else{
-            return (likes + dislikes) / likes;
+            return likes / (likes + dislikes);
+        }
+
+    }
+
+    @Override
+    public double getUserRating(String userName) {
+        try {
+            return entityManager.createQuery("Select avg(rating) from Comment c where c.userName = :userName", Double.class).setParameter("userName",userName).getSingleResult();
+        }catch (NoResultException e){
+            return 0.5;
         }
     }
 }
