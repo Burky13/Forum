2acc05201a172be9c288b3358c11c31d821c2831
Burky13 <jan.burkus@gmail.com>
2018-09-18 10:29:55 +0200

    delete comment theme and category

--- forum/src/main/java/forum/controllers/ThemeController.java
+++ forum/src/main/java/forum/controllers/ThemeController.java
@@ -45,7 +45,7 @@ public class ThemeController {
         if(id != null){
             themeService.deleteTheme(id);
         }
-        return "theme";
+        return "redirect:/";
     }
 
     public Theme getActualTheme() {
--- forum/src/main/java/forum/services/Category/CategoryService.java
+++ forum/src/main/java/forum/services/Category/CategoryService.java
@@ -10,4 +10,5 @@ public interface CategoryService {
     List<Category> getAllCategories();
     void addCategory(Category category);
     Category getCategory(Long id);
+    void deleteCategory(Long id);
 }
